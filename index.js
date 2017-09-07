6881cb644723d7e4afb2b1f8a89c0d7284aac136
NatashaKSS <xnagol@gmail.com>
2017-09-07 23:32:34 +0800

    Added request func for restaurants nearby FB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express(),
+    dotenv = require('dotenv').config(),
     bodyParser = require('body-parser'),
     path = require('path'),
-    request = require('request');
+    request = require('request'),
+    json2csv = require('json2csv');
 
 const RADIUS = 500;
 
@@ -13,18 +15,48 @@ const JURONG_EAST_COORD = [1.3339485, 103.7421645];
  * Set up application middleware
  */
 function setup() {
-  console.info("Setting up middleware...");
+  console.info('SETTING UP MIDDLEWARE');
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: true
   }));
 }
 
-function getRestaurantsNearbyFromFB(lat, long, radius) {
-  //...
+function getFBRestaurantsURL(lat, long, radius) {
+  const BASE_URL = 'https://graph.facebook.com/v2.10/search?q=&';
+  const TYPE = 'type=place&';
+  const CENTER = 'center=' + lat + ',' + long + '&';
+  const DISTANCE = 'distance=' + radius + '&';
+  const LIMIT = 'limit=25&';
+  const CATEGORIES = "categories=['FOOD_BEVERAGE']&";
+  const ACCESS_TOKEN = 'access_token=' + process.env.FB_ACCESS_TOKEN + '&';
+  const FIELDS = 'fields=name,about,description,name_with_location_descriptor,store_location_descriptor,category,category_list,place_type,current_location,location,hours,price_range,food_styles,overall_star_rating,restaurant_services,restaurant_specialties,parent_page';
+
+  return BASE_URL + TYPE + CENTER + DISTANCE + LIMIT + CATEGORIES + ACCESS_TOKEN + FIELDS;
 }
 
-/**
- * Running application code
- */
+
+function getFBRestaurantsNearby(url) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error('ERROR occurred while retrieving restaurant nodes from FB:', error);
+    }
+
+    if (response && response.statusCode == 200) {
+      console.log('SUCCESS, Status code: ', response.statusCode);
+    }
+
+    console.log('=== Body Received ===');
+    result = JSON.parse(body);
+    console.log(result);
+    console.log('\n=================');
+    console.log('=== TOTAL: ' + result.data.length + ' ===');
+    console.log('=================');
+  });
+}
+
+//===================
+// RUN
+//===================
 setup();
+getFBRestaurantsNearby(getFBRestaurantsURL(1.3339485,103.7421645, 500));
