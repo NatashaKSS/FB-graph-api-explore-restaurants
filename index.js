56df4abac98dfd720fbf33e480374aa21dea73f7
NatashaKSS <xnagol@gmail.com>
2017-09-07 19:46:16 +0800

    Added base code to import dependencies & setup middleware

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello world!")
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    request = require('request');
+
+const RADIUS = 500;
+
+/* Latitude, Longitude tuples */
+const JURONG_EAST_COORD = [1.3339485, 103.7421645];
+
+/**
+ * Set up application middleware
+ */
+function setup() {
+  console.info("Setting up middleware...");
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+}
+
+function getRestaurantsNearbyFromFB(lat, long, radius) {
+  //...
+}
+
+/**
+ * Running application code
+ */
+setup();
